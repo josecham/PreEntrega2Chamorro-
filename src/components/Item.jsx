d72431897daffa826fe83c448fd98c45e1a71ba2
josecham <gabriel_influencia74@hotmail.com>
2023-09-02 13:36:48 -0300

    la etiqueta de lo que guardo

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Card, CardBody, CardFooter, Image, Stack, Heading, Divider, ButtonGroup, Button,Text } from '@chakra-ui/react'
-
+import { Grid } from '@chakra-ui/react'
 
 const Item = ({producto}) => {
   return (
   <>
    <div className="titulo" >
-   
+   <Grid templateColumns="repeat(3, 1fr)" gap={4}>
   <Card maxW='sm'>
   <CardBody>
+    <div className="foto" >
     <Image
       src={producto.image}
       borderRadius='lg'
     />
+    </div>
     <Stack mt='6' spacing='3'>
       <Heading size='md'>{producto.title}</Heading>
       <Text>
@@ -32,7 +34,7 @@ const Item = ({producto}) => {
     </ButtonGroup>
   </CardFooter>
 </Card>
-
+</Grid>
 </div>
 </>
   )
