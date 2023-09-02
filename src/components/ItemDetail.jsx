b1f880bb35d5d312844fe89489c5f96755a5c797
josecham <gabriel_influencia74@hotmail.com>
2023-09-01 22:43:51 -0300

    la etiqueta de lo que guardo

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import ItemCount from './ItemCount'
-import { Grid } from '@chakra-ui/react';
+import { Card, CardBody, CardFooter, Stack, Heading, Text, Divider, Image, Button } from '@chakra-ui/react';
+
+
 
 const ItemDetail = ({producto}) => {
   return (
-    <>
-    <div className="item-detail">
-      <Grid templateColumns="repeat(3, auto)" gap={4}> {/* Define la disposición de 3 columnas */}
-        
-      <Card maxW='sm'>
-      <Image src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60' alt={producto.nombre} />
-        <CardBody>
-          <Stack spacing="1rem">
-            <Heading as="h2" size="md">
-            {producto.nombre}
-            </Heading>
-            <Text>{producto.descripcion}</Text>
-            <Text className="precio">Precio: ${producto.precio}</Text>
-          </Stack>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <ItemCount/>
-        </CardFooter>
-      </Card>
-      </Grid>
-      </div>
-      </>
-    );
-  };
+<>
+<Card maxW='sm'>
+      <Image src= 'https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'alt={producto.nombre} />
+      <CardBody>
+        <Stack spacing="1rem">
+          <Heading as="h2" size="md">
+          {producto.nombre}
+          </Heading>
+          <Text>{producto.descripcion}</Text>
+          <Text className="precio">Precio: ${producto.precio}</Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <ButtonGroup spacing="2">
+          <Button variant="solid" colorScheme="blue">
+            Detalle
+          </Button>
+        </ButtonGroup>
+        <ItemCount/>
+      </CardFooter>
+    </Card>
+ 
+    
+    </>
+  )
+}
 
 export default ItemDetail
