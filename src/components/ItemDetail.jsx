b2bfd04485cd6ca1b53c4fb0c78f7c3fb57ce939
josecham <gabriel_influencia74@hotmail.com>
2023-09-02 11:28:50 -0300

    la etiqueta de lo que guardo

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -5,42 +5,42 @@ import ItemCount from './ItemCount'
 
 const ItemDetail = ({producto}) => {
   return (
-    <Card maxW='sm'>
-  <CardBody>
-    <Image
-      src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-      alt='Green double couch with wooden legs'
-      borderRadius='lg'
-    />
-    <Stack mt='6' spacing='3'>
-      <Heading size='md'>Living room Sofa</Heading>
-      <Text>
-        This sofa is perfect for modern tropical spaces, baroque inspired
-        spaces, earthy toned spaces and for people who love a chic design with a
-        sprinkle of vintage design.
-      </Text>
-      <Text color='blue.600' fontSize='2xl'>
-        $450
-      </Text>
-    </Stack>
-  </CardBody>
-  <Divider />
-  <CardFooter>
-    <ButtonGroup spacing='2'>
-      <Button variant='solid' colorScheme='blue'>
-        Buy now
-      </Button>
-      <Button variant='ghost' colorScheme='blue'>
-        Add to cart
-      </Button>
-    </ButtonGroup>
-  </CardFooter>
-</Card>
+    <>
+    <div  className="item-detail titulo">
+      
+    <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+      <Card maxW='sm'>
+      <Image src={producto.image} />
+        <CardBody>
+          <Stack spacing="1rem">
+            <Heading as="h2" size="md">
+            {producto.title}
+            </Heading>
+            <Text>{producto.description}</Text>
+            <Text className="precio">Precio: ${producto.price}</Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <ItemCount/>
+        <CardFooter>
+        
+        <ButtonGroup spacing="2">
+          <Button variant="solid" colorScheme="blue">
+            Detalle
+          </Button>
+          
+        </ButtonGroup>
+        
+        </CardFooter>
+       
+      </Card>
+      </Grid>
+      </div>
+      </>
     );
   };
   
  
-  
 
 
 export default ItemDetail
