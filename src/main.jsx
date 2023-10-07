9434a552bb30ac4646e05ec19870c9ff70ba87fc
josecham <gabriel_influencia74@hotmail.com>
2023-10-06 23:29:32 -0300

    la etiqueta de lo que guardo

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -10,12 +11,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBuQwpTkc6H-G7LPjhQ8H5bfp2lt04ZhO8",
-  authDomain: "dc-comics-fe3ef.firebaseapp.com",
-  projectId: "dc-comics-fe3ef",
-  storageBucket: "dc-comics-fe3ef.appspot.com",
-  messagingSenderId: "727379657875",
-  appId: "1:727379657875:web:a127905af6be82dc43f966"
+  //SDK de conexion
 };
 
 // Initialize Firebase
